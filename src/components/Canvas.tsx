2fb608500e2721f2d073f104624e179165b81ed6
James <jamesbrass@ymail.com>
2026-09-11 09:59:59 +0100

    can now drag and drop nodes of selected service

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -8,11 +8,16 @@ const nodes = [
   { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
 ];
 
+
+const ghostNodes: { id: string; position: { x: number; y: number; }; data: { label: string; } }[] = [];
+
 const edges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
  
 export default function Canvas({ selectedService, setSelectedService }: { selectedService: string | null; setSelectedService: (service: string | null) => void }) {
 
   const [stateNodes, setNodes] = useState(nodes);
+  const [stateGhostNodes, setGhostNodes] = useState(ghostNodes);
   const [stateEdges, setEdges] = useState(edges);
   const { screenToFlowPosition } = useReactFlow();
  
@@ -23,22 +28,20 @@ export default function Canvas({ selectedService, setSelectedService }: { select
   return (
     <div className="reactFlowWrapper">
       <ReactFlow
-        nodes={stateNodes}
+        nodes={[...stateNodes, ...stateGhostNodes]}
         edges={stateEdges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
-        onPaneClick={(event) => handlePaneClick(event, screenToFlowPosition, selectedService, setSelectedService, stateNodes, setNodes)}
-        onPaneMouseMove={(event) => handlePaneMouseMove(event, screenToFlowPosition, selectedService, setSelectedService)}
+        onPaneClick={(event) => handlePaneClick(event, screenToFlowPosition, selectedService, setSelectedService, stateNodes, setNodes, setGhostNodes)}
+        onPaneMouseMove={(event) => handlePaneMouseMove(event, screenToFlowPosition, selectedService, setGhostNodes )}
       />
     </div>
   );
 }
 
-function handlePaneClick(event: React.MouseEvent, screenToFlowPosition: ViewportHelperFunctions['screenToFlowPosition'], selectedService: string | null, setSelectedService: (service: string | null) => void, stateNodes: any[], setNodes: (nodes: any[]) => void) {
-
-  selectedService != null ? console.log(`Place service node ${selectedService}`) : console.log('Dont place service node');
+function handlePaneClick(event: React.MouseEvent, screenToFlowPosition: ViewportHelperFunctions['screenToFlowPosition'], selectedService: string | null, setSelectedService: (service: string | null) => void, stateNodes: any[], setNodes: (nodes: any[]) => void, setGhostNodes: (nodes: any[]) => void) {
 
   if(selectedService == null) {
     return;
@@ -48,13 +51,21 @@ function handlePaneClick(event: React.MouseEvent, screenToFlowPosition: Viewport
   const newNode = { id: `n${stateNodes.length + 1}`, position: flowPosition, data: { label: selectedService ?? `${selectedService} ID:${stateNodes.length + 1}` } };
 
   setNodes([...stateNodes, newNode]);
+  setGhostNodes([]);
 
   setSelectedService(null);
 
 }
 
-function handlePaneMouseMove(event: React.MouseEvent, screenToFlowPosition: ViewportHelperFunctions['screenToFlowPosition'], selectedService: string | null, setSelectedService: (service: string | null) => void) {
+function handlePaneMouseMove(event: React.MouseEvent, screenToFlowPosition: ViewportHelperFunctions['screenToFlowPosition'], selectedService: string | null, setGhostNodes: (nodes: any[]) => void){
+
+  if(selectedService == null) {
+    return;
+  }
 
   const flowPosition = screenToFlowPosition({ x: event.clientX, y: event.clientY });
 
+  const newGhostNode = { id: `ghost-${selectedService}`, position: flowPosition, data: { label: selectedService }, measured: { width: 300, height: 72 } };
+  setGhostNodes([newGhostNode]);
+  
 }
